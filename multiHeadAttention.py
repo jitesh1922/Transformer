496e53ec6a7e671668c43d3dc9093c6fff23a073
Jitesh Kumar dewangan <jiteshdewangan99@gmail.com>
2025-03-26 15:06:52 +0530

    Update multiHeadAttention.py

--- multiHeadAttention.py
+++ multiHeadAttention.py
@@ -12,6 +12,8 @@ class MultiHeadAttention(nn.Module):
         self.W_o = nn.Linear(d_model, d_model)
         
     def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        ##K.transpose(-2, -1) swaps the last two dimensions, making K shape [..., d_k, seq_length].
+        #Result: [batch_size, num_heads, seq_length, seq_length] (attention scores
         attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
         if mask is not None:
             attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
@@ -19,12 +21,17 @@ class MultiHeadAttention(nn.Module):
         output = torch.matmul(attn_probs, V)
         return output
         
-    def split_heads(self, x):
+    def split_heads(self, x): # Input: x of shape [batch_size, seq_length, d_model].
         batch_size, seq_length, d_model = x.size()
+        ##Reshapes to [batch_size, seq_length, num_heads, d_k]
+        #transpose(1, 2): Swaps dimensions 1 and 2, resulting in [batch_size, num_heads, seq_length, d_k].
         return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
         
     def combine_heads(self, x):
         batch_size, _, seq_length, d_k = x.size()
+        #transpose(1, 2): Swaps back to [batch_size, seq_length, num_heads, d_k].
+        ##contiguous(): Ensures memory layout is continuous (required after transpose for view).
+        #view(...): Flattens heads back into [batch_size, seq_length, d_model].
         return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.d_model)
         
     def forward(self, Q, K, V, mask=None):
